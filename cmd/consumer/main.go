a5a04550dee59cfc6572036ac8cb5fb665ddd35d
ragul28 <ragulh28@gmail.com>
2020-11-08 23:00:44 +0530

    Add cmd to generate separate binaries for producer & consumer

---
+++ cmd/consumer/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+
+	"github.com/ragul28/rabbitmq-benchmark/queue"
+	"github.com/ragul28/rabbitmq-benchmark/utils"
+)
+
+var rabbitURL string
+var numWorker int
+
+func main() {
+
+	flag.StringVar(&rabbitURL, "url", "amqp://guest:guest@localhost:5672", "Rabbitmq connection string")
+	flag.IntVar(&numWorker, "wt", 3, "Num of worker threads")
+
+	flag.Parse()
+
+	// Start consumer worker threads using goroutine
+	for w := 1; w <= numWorker; w++ {
+		ch, q := queue.InitRabbitMQ(rabbitURL)
+		fmt.Printf("Consumer Worker %d started..\n", w)
+		go queue.ConsumerMQ(ch, q)
+	}
+
+	utils.CloserHandler()
+	// Block main thread to allow running goroutines
+	select {}
+}
---
+++ cmd/producer/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+
+	"github.com/ragul28/rabbitmq-benchmark/queue"
+	"github.com/ragul28/rabbitmq-benchmark/utils"
+)
+
+var rabbitURL string
+var numWorker int
+
+func main() {
+
+	flag.StringVar(&rabbitURL, "url", "amqp://guest:guest@localhost:5672", "Rabbitmq connection string")
+	flag.IntVar(&numWorker, "wt", 3, "Num of worker threads")
+
+	flag.Parse()
+
+	// Start publisher worker threads using goroutine
+	for w := 1; w <= numWorker; w++ {
+		ch, q := queue.InitRabbitMQ(rabbitURL)
+		fmt.Printf("Publisher Worker %d started..\n", w)
+		go queue.PublishMQ(ch, q)
+	}
+
+	utils.CloserHandler()
+	// Block main thread to allow running goroutines
+	select {}
+}
