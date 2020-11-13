4081688a138efc14d43a6587c741bc4397755f14
ragul28 <ragulh28@gmail.com>
2020-11-13 21:48:06 +0530

    Added quorum queue type, debug logs & massage frequency control

--- queue/consumer.go
+++ queue/consumer.go
@@ -7,16 +7,24 @@ import (
 )
 
 // ConsumerMQ consume queue messages
-func ConsumerMQ(ch *amqp.Channel, q amqp.Queue) {
+func ConsumerMQ(ch *amqp.Channel, q amqp.Queue, enableQuorum bool, enableDebug bool) {
+
+	var queueArgs amqp.Table = nil
+
+	if enableQuorum {
+		queueArgs = amqp.Table{
+			"x-queue-type": "quorum",
+		}
+	}
 
 	msgs, err := ch.Consume(
-		q.Name, // queue
-		"",     // consumer
-		false,  // auto-ack
-		false,  // exclusive
-		false,  // no-local
-		false,  // no-wait
-		nil,    // args
+		q.Name,    // queue
+		"",        // consumer
+		false,     // auto-ack
+		false,     // exclusive
+		false,     // no-local
+		false,     // no-wait
+		queueArgs, // args
 	)
 
 	if err != nil {
@@ -27,7 +35,9 @@ func ConsumerMQ(ch *amqp.Channel, q amqp.Queue) {
 
 	go func() {
 		for d := range msgs {
-			// log.Printf("consumer %d message: %s", numWorker, d.Body)
+			if enableDebug {
+				log.Printf("consumer message: %s", d.Body)
+			}
 			d.Ack(false)
 		}
 	}()
