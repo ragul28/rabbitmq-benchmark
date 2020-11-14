1baa3dd9d758d2fbd37a0512c4f3188632bfa270
ragul28 <ragulh28@gmail.com>
2020-11-14 16:55:42 +0530

    Added failuer retry for consumer help of connection notify channel

--- queue/consumer.go
+++ queue/consumer.go
@@ -6,8 +6,8 @@ import (
 	"github.com/streadway/amqp"
 )
 
-// ConsumerMQ consume queue messages
-func ConsumerMQ(ch *amqp.Channel, q amqp.Queue, enableQuorum bool, enableDebug bool) {
+// consumer consume queue messages
+func consumer(ch *amqp.Channel, q amqp.Queue, enableQuorum bool) (<-chan amqp.Delivery, error) {
 
 	var queueArgs amqp.Table = nil
 
@@ -33,14 +33,43 @@ func ConsumerMQ(ch *amqp.Channel, q amqp.Queue, enableQuorum bool, enableDebug b
 
 	forever := make(chan bool)
 
-	go func() {
-		for d := range msgs {
-			if enableDebug {
-				log.Printf("consumer message: %s", d.Body)
+// ConsumerMQ worker func
+func ConsumerMQ(cfg utils.ConfigStore) {
+	ch, q, notify, err := InitRabbitMQ(cfg.RabbitURL, cfg.QueueName, cfg.EnableQuorum)
+	if err != nil {
+		log.Fatal(err)
+	}
+	msgs, _ := consumer(ch, q, cfg.EnableQuorum)
+
+	// forever := make(chan bool)
+	var d amqp.Delivery
+	// d := make(chan amqp.Delivery)
+
+	for { //receive loop
+		select { //check connection
+		case <-notify:
+			fmt.Println("Detects connection failuer, retring..")
+			ch, q, notify, msgs = failuerRetry(cfg)
+		case d = <-msgs:
+			if cfg.EnableDebug {
+				log.Printf("consumer message: %s\n", d.Body)
 			}
-			d.Ack(false)
 		}
+	}
+			d.Ack(false)
+}
 	}()
 
-	<-forever
+func failuerRetry(cfg utils.ConfigStore) (*amqp.Channel, amqp.Queue, chan *amqp.Error, <-chan amqp.Delivery) {
+	for {
+		ch, q, notify, err := InitRabbitMQ(cfg.RabbitURL, cfg.QueueName, cfg.EnableQuorum)
+		if err != nil {
+			log.Println("Sleep 15 sec before retrying the publish")
+			time.Sleep(15 * time.Second)
+		} else {
+			fmt.Println("Reconnection is successful.")
+			msgs, _ := consumer(ch, q, cfg.EnableQuorum)
+			return ch, q, notify, msgs
+		}
+	}
 }
